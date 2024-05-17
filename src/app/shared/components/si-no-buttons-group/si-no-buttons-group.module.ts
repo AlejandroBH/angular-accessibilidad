bafba15c54c315c047018c58563fe0177be6146c
Alejandro Barrera <alejandro91dark@hotmail.es>
2024-05-16 22:51:40 -0400

    :sparkles: Clase04 completada

--- src/app/shared/components/si-no-buttons-group/si-no-buttons-group.module.ts
+++ src/app/shared/components/si-no-buttons-group/si-no-buttons-group.module.ts
@@ -1,10 +1,11 @@
+import { KeyboardManagerModule } from './../../directives/keyboard-manager/keyboard-manager.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SiNoButtonsGroupComponent } from './si-no-buttons-group.component';
 
 @NgModule({
   declarations: [SiNoButtonsGroupComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, KeyboardManagerModule],
   exports: [SiNoButtonsGroupComponent],
 })
 export class SiNoButtonsGroupModule {}
